cff1ae8498396eeabc07c2899c01c659f765900d
monty magbanua <montgomery@ideahubsolutionsinc.com>
2018-07-16 17:57:58 +0800

    smoothscroll header

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -17,3 +17,23 @@ $(".navbar-collapse ul li a[href^='#']").on('click', function(e) {
      });
 
 });
+
+$("a.navbar-brand[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 700, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
